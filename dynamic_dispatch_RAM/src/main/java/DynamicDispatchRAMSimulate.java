f2bdb558b7f006e206968b111cf042053c26abd3
fangzidong <424044876@qq.com>
2020-11-29 15:32:25 +0800

    process_simulate_lombok: still have some bugs

--- dynamic_dispatch_RAM/src/main/java/DynamicDispatchRAMSimulate.java
+++ dynamic_dispatch_RAM/src/main/java/DynamicDispatchRAMSimulate.java
@@ -1,3 +1,4 @@
+import com.sun.tools.internal.ws.wsdl.document.soap.SOAPUse;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -26,6 +27,7 @@ class Job {
     // 0 for ready, 1 for over
     private int status;
     private int needMemory;
+    private int address;
 }
 
 public class DynamicDispatchRAMSimulate {
@@ -45,7 +47,7 @@ public class DynamicDispatchRAMSimulate {
         return begin + (int) (Math.random() / segment);
     }
 
-    // default for
+    // default for 10, 1024
     public static List<MemoryBlock> initMemoryBlocks(int memoryBlockLength, int memoryLength) {
         List<MemoryBlock> memoryBlockList = new ArrayList<MemoryBlock>();
         int initialDenominator = 512;
@@ -55,15 +57,150 @@ public class DynamicDispatchRAMSimulate {
             addressTemp += currentBlockLength;
             initialDenominator /= 2;
         }
+        MemoryBlock lastBlock = memoryBlockList.get(memoryBlockList.size() - 1);
+        lastBlock.setLength(memoryLength - lastBlock.getAddress() + 1);
         return memoryBlockList;
     }
 
-    public static List<Job> initJobs(int jobNum) {
+    // default for 10, 1024
+    public static List<Job> initJobs(int jobNum, int memoryLength) {
         List<Job> jobList = new ArrayList<Job>();
-
+        for (int i = 0; i < jobNum; i++) {
+            jobList.add(new Job("job " + i, 0, getRandom(1, memoryLength), -1));
+        }
+        return jobList;
     }
 
-    public static void main(String[] args) {
+    public static void showJobList(List<Job> jobList) {
+        for (Job i : jobList) {
+            if (i.getStatus() == 0) {
+                System.out.print(i.getName() + ": " + i.getAddress() + "Byte, ");
+            }
+        }
+        System.out.println();
+    }
 
+    public static void showMemoryList(List<MemoryBlock> memoryBlockList){
+        for (MemoryBlock i:memoryBlockList){
+            if (i.getFlag()==0){
+                System.out.print("|"+i.getAddress() + " ~ " + (i.getAddress() + i.getLength() - 1)+"|");
+            }else {
+                System.out.print("[" + i.getAddress() + " ~ " + (i.getAddress() + i.getLength() - 1) + "]");
+            }
+        }
+        System.out.println();
+    }
+
+    public static int FFLocate(List<MemoryBlock> memoryBlockList, Job job, int cursor) {
+        int currentCursor = cursor;
+        int initialCursor = cursor;
+        while (true) {
+            MemoryBlock currentBlock = memoryBlockList.get(currentCursor);
+            if (currentBlock.getFlag() == 0 && job.getNeedMemory() <= currentBlock.getLength()) {
+                return currentCursor;
+            } else {
+                currentCursor = (currentCursor + 1) % memoryBlockList.size();
+            }
+            if (currentCursor == initialCursor) {
+                return -1;
+            }
+        }
+    }
+
+    public static boolean allocate(List<MemoryBlock> memoryBlockList, Job job, int allocatePosition) {
+        MemoryBlock allocateBlock = memoryBlockList.get(allocatePosition);
+        job.setAddress(allocateBlock.getAddress());
+        if (allocateBlock.getLength() == job.getNeedMemory()) {
+            allocateBlock.setFlag(1);
+        } else if (allocateBlock.getLength() > job.getNeedMemory()) {
+            MemoryBlock remainBlock = new MemoryBlock(
+                    allocateBlock.getAddress() + job.getNeedMemory(),
+                    allocateBlock.getLength() - job.getNeedMemory(),
+                    0);
+            memoryBlockList.add(allocatePosition+1, remainBlock);
+        } else {
+            return false;
+        }
+        return true;
+    }
+
+
+
+    public static boolean free(List<MemoryBlock> memoryBlockList, Job job) {
+        int position=-1;
+        for (int i=0; i<memoryBlockList.size();i++){
+            if (job.getAddress()==memoryBlockList.get(i).getAddress()){
+                position = i;
+                break;
+            }
+        }
+        if (position==-1){
+            return false;
+        }
+
+        job.setStatus(1);
+
+        if (position - 1 >= 0 && memoryBlockList.get(position - 1).getFlag() == 0) {
+            MemoryBlock preBlock = memoryBlockList.get(position - 1);
+            preBlock.setLength(memoryBlockList.get(position).getLength() + preBlock.getLength());
+            memoryBlockList.remove(position);
+            position--;
+        }
+        if (position + 1 < memoryBlockList.size() && memoryBlockList.get(position + 1).getFlag() == 0) {
+            MemoryBlock postBlock = memoryBlockList.get(position + 1);
+            memoryBlockList.get(position).setLength(memoryBlockList.get(position).getLength() +
+                    postBlock.getLength());
+            memoryBlockList.remove(position + 1);
+        }
+        memoryBlockList.get(position).setFlag(0);
+        return true;
+    }
+
+    // FF denote for first fit
+    public static void FF(List<MemoryBlock> memoryBlockList, List<Job> jobList) {
+        int totalJobNum = jobList.size();
+        List<Job> runningJobs = new ArrayList<Job>();
+        List<Job> overJobs = new ArrayList<Job>();
+        List<Job> waitingJobs = jobList;
+        while (overJobs.size() < totalJobNum) {
+            int cursor = 0;
+            for (int i = 0; i < waitingJobs.size(); ) {
+                int backCursor = FFLocate(memoryBlockList, waitingJobs.get(i), cursor);
+                if (backCursor==-1){
+                    i++;
+                }else {
+                    allocate(memoryBlockList, waitingJobs.get(i), backCursor);
+                    cursor = ++backCursor;
+                    runningJobs.add(waitingJobs.get(i));
+                    waitingJobs.remove(i);
+                }
+            }
+
+            System.out.println("***************************************");
+            System.out.println("running jobs: ");
+            showJobList(runningJobs);
+            System.out.println("waiting jobs: ");
+            showJobList(waitingJobs);
+            System.out.println("over jobs: ");
+            showJobList(overJobs);
+            showMemoryList(memoryBlockList);
+
+            for (int i=0; i<runningJobs.size();i++){
+                if (getRandom()==1){
+                    free(memoryBlockList, runningJobs.get(i));
+                    overJobs.add(runningJobs.get(i));
+                    runningJobs.remove(i);
+                }else {
+                    i++;
+                }
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        List<MemoryBlock> memoryBlockList = initMemoryBlocks(10, 1024);
+        List<Job> jobList = initJobs(10, 1024);
+        FF(memoryBlockList, jobList);
     }
 }
